70ebd8809ad057ffcb793a5cb345909baabc994f
nguyenquangnhat1208 <nguyenquangnhat120898@gmail.com>
2019-03-06 22:46:49 +0700

    nop bai tap

--- Session03/Homework/jumble.py
+++ Session03/Homework/jumble.py
@@ -15,25 +15,4 @@ while jumble == True:
         jumble = False
     else:
         print("Incorrect. Try again")
-# import random
 
-# word_list = ["beast","hunter","warrior","warlock","undead","goblin","druid","assassin"]
-
-# first_quiz = random.choice(word_list)
-# shuffle = list(first_quiz)
-# random.shuffle(shuffle)
-
-# correct = first_quiz
-
-# for i in range(0,len(first_quiz)):
-#     print(shuffle[i],end=' ')
-# print()
-# answer = input('Your answer: ')
-
-# while True:
-    # if answer == correct:
-#         print('Dân chơi cờ nhân phẩm thứ thiệt =)))')
-#         break
-#     else:
-#         print('Bạn  đã chơi cờ nhân phẩm chưa, nếu chưa hãy chơi để biết đáp án :)')
-#         answer = input('Your answer: ')
